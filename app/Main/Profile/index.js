111d6b90965ea7cc77ec1843157a6edf4ee11887
Husein Nabil <seinnabil@gmail.com>
2022-11-01 12:25:08 +0700

    Profile Screen

--- app/Main/Profile/index.js
+++ app/Main/Profile/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, ImageBackground, Image} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
 import styles from './styles';
 import {useDispatch} from 'react-redux';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <ImageBackground
+      source={require('assets/bg.png')}
+      resizeMode={'cover'}
+      style={styles.imageBg}>
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            flex: 1 / 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text style={styles.title}>Profile</Text>
+        </View>
+        <View
+          style={{
+            flex: 5 / 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            resizeMode={'contain'}
+            source={require('@assets/images/photo.jpg')}
+            style={styles.photo}
+          />
+        </View>
+        <View
+          style={{
+            flex: 3.5 / 10,
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}>
+          <Text style={styles.profile}>Marital status: Single</Text>
+          <Text style={styles.profile}>Nationality: Indonesia</Text>
+          <Text style={styles.profile}>Gender: Male</Text>
+          <Text style={styles.profile}>Place of Birth: Malang</Text>
+          <Text style={styles.profile}>Date of Birth: February 10th, 1997</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
--- app/Main/Profile/styles.js
+++ app/Main/Profile/styles.js
@@ -3,4 +3,23 @@ import {Dimensions, StyleSheet} from 'react-native';
 const FullWidth = Dimensions.get('window').width;
 const FullHeight = Dimensions.get('window').height;
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  photo: {
+    height: 250,
+  },
+  profile: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+    marginTop: 10,
+    textAlign: 'left',
+  },
+  imageBg: {
+    flex: 1,
+  },
+});
